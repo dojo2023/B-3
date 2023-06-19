10fa8c1e83a19babc7eb971f5c69b634b6133177
dojo2023-komori <komoritakaeplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-19 17:35:21 +0900

    小森_6/19_後半分

--- src/src/servlet/ReserveServlet.java
+++ src/src/servlet/ReserveServlet.java
@@ -2,6 +2,7 @@ package servlet;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,30 +15,54 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/ReserveServlet")
 public class ReserveServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ReserveServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-		
+
+	/*	// もしもログインしていなかったらログインサーブレットにリダイレクトする
+			HttpSession session = request.getSession();
+			if (session.getAttribute("id") == null) {
+				response.sendRedirect("/hydrangea/LoginServlet");
+				return;
+			}*/
+
+			//リクエストパラメータの取得
+			request.setCharacterEncoding("UTF-8");
+			String shop_name = request.getParameter("shop_name");
+
+			//予約ページにフォワードする
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/reserve.jsp");
+				dispatcher.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+
+	/*	// もしもログインしていなかったらログインサーブレットにリダイレクトする
+			HttpSession session = request.getSession();
+			if (session.getAttribute("id") == null) {
+				response.sendRedirect("/hydrangea/LoginServlet");
+				return;
+			}*/
+
+			//リクエストパラメータの取得
+			request.setCharacterEncoding("UTF-8");
+			String id_shops = request.getParameter("shop_name");
+			String shop_name = request.getParameter("shop_name");
+			String apointment_date = request.getParameter("apointment_date");
+			String apointment_time = request.getParameter("apointment_time");
+			String department = request.getParameter("department");
+			String people = request.getParameter("people");
+			String remarks_reserve = request.getParameter("remarks_reserve");
+
+			//カレンダーページにフォワードする
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/calendar.jsp");
+			dispatcher.forward(request, response);
 	}
 
 }
--- src/src/servlet/ShopDetailServlet.java
+++ src/src/servlet/ShopDetailServlet.java
@@ -11,7 +11,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+
 import dao.ShopDAO;
+import model.Shop;
 
 /**
  * Servlet implementation class ShopDetailServlet
@@ -33,15 +35,62 @@ public class ShopDetailServlet extends HttpServlet {
 			return;
 		}
 
+		//リクエストパラメータの取得
+		request.setCharacterEncoding("UTF-8");
+		String shop_name = request.getParameter("shop_name");
+		String genre = request.getParameter("genre");
+		String genre_form = request.getParameter("genre_form");
+		String price_max = request.getParameter("price_max");
+		String price_min = request.getParameter("price_min");
+		String open_hei = request.getParameter("open_hei");
+		String close_hei = request.getParameter("close_hei");
+		String open_kyu = request.getParameter("open_kyu");
+		String close_kyu = request.getParameter("close_kyu");
+		String holiday_mon = request.getParameter("holiday_mon");
+		String holiday_tue = request.getParameter("holiday_tue");
+		String holiday_wed = request.getParameter("holiday_wed");
+		String holiday_thu = request.getParameter("holiday_thu");
+		String holiday_fri = request.getParameter("holiday_fri");
+		String holiday_sat = request.getParameter("holiday_sat");
+		String holiday_sun = request.getParameter("holiday_sun");
+		String holiday_syuku = request.getParameter("holiday_syuku");
+		String holiday_nenmatsu = request.getParameter("holiday_nenmatsu");
+		String holiday_other = request.getParameter("holiday_other");
+		String distance = request.getParameter("distance");
+		String address = request.getParameter("address");
+		String tabaco = request.getParameter("tabaco");
+		String sheet_table = request.getParameter("sheet_table");
+		String sheet_tatami = request.getParameter("sheet_tatami");
+		String sheet_other = request.getParameter("sheet_other");
+		String capacity = request.getParameter("capacity");
+		String eat_drink = request.getParameter("eat_drink");
+		String score = request.getParameter("score");
+		String tel = request.getParameter("tel");
+		String homepage = request.getParameter("homepage");
+		String other = request.getParameter("other");
+		String photo = request.getParameter("photo");
+		String remarks_shop = request.getParameter("remarks_shop");
+
+		// 検索処理を行う
+		if (request.getParameter("SUBMIT").equals("検索")) {
+		ShopsDAO bDao = new ShopsDAO(); //DB,listしてね 確認6/16
+		List<Bc> cardList =
+			bDao.select(new Bc(shop_name, genre, genre_form, price_max,price_min, open_hei, close_hei, open_kyu, close_kyu,
+			holiday_mon,holiday_tue, holiday_wed, holiday_thu, holiday_fri, holiday_sat, holiday_sun, holiday_syuku, holiday_nenmatsu, holiday_other,
+			distance, address, tabaco, sheet_table, sheet_tatami, sheet_other, capacity, eat_drink, score, tel, homepage, other, photo, remarks_shop));
+
+		// 検索結果をリクエストスコープに格納する
+		request.setAttribute("cardList", cardList);//リスト名確認
+
+		// 結果ページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/serchresult.jsp");
+		dispatcher.forward(request, response);
+		}else {
 		// 一覧表示処理を行う
 		Shop bDao = new ShopDAO();
 		List<Bc> detaList = bDao.findAll();//
 
-		// 検索結果をリクエストスコープに格納する
-		request.setAttribute("detaList", detaList);
-		// 一覧ページにフォワードする
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/shopdetail.jsp");
-		dispatcher.forward(request,response);
+
 	}
 
 
@@ -55,6 +104,28 @@ public class ShopDetailServlet extends HttpServlet {
 					response.sendRedirect("/hydrangea/LoginServlet");
 					return;
 				}
+
+				// 更新を行う
+				BcDAO bDao = new BcDAO();
+				if (request.getParameter("SUBMIT").equals("更新")) {
+
+						RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/UpdateServlt.jsp");
+						dispatcher.forward(request, response);
+					}
+				//削除を行う
+					else if(request.getParameter("SUBMIT").equals("削除申請")) {
+						RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/DeleteServlt.jsp");
+						dispatcher.forward(request, response);
+					}
+				}
+				//開催する
+				else if(request.getParameter("SUBMIT").equals("開催する")){
+					RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/ReserveServlt.jsp");
+					dispatcher.forward(request, response);
+					}
+
+				}
+
 	}
 
-}
+
