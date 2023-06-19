75f5e460b1e4b7bb84667b80dac9f1a2f50e1db7
dojo2023-komori <komoritakaeplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-19 12:02:37 +0900

    小森担当分_6/19_前半

--- src/src/servlet/ShopDetailServlet.java
+++ src/src/servlet/ShopDetailServlet.java
@@ -1,11 +1,17 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.ShopDAO;
 
 /**
  * Servlet implementation class ShopDetailServlet
@@ -13,29 +19,42 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/ShopDetailServlet")
 public class ShopDetailServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ShopDetailServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		HttpSession session = request.getSession();
+
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		if (session.getAttribute("id") == null) {
+			response.sendRedirect("/hydrangea/LoginServlet");
+			return;
+		}
+
+		// 一覧表示処理を行う
+		Shop bDao = new ShopDAO();
+		List<Bc> detaList = bDao.findAll();//
+
+		// 検索結果をリクエストスコープに格納する
+		request.setAttribute("detaList", detaList);
+		// 一覧ページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/shopdetail.jsp");
+		dispatcher.forward(request,response);
 	}
+
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+				HttpSession session = request.getSession();
+				if (session.getAttribute("id") == null) {
+					response.sendRedirect("/hydrangea/LoginServlet");
+					return;
+				}
 	}
 
 }
