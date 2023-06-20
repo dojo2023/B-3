c874a9c2025fb4dcecbe5b1ec11ab7e095400552
dojo2023-seki <130331170+dojo2023-seki@users.noreply.github.com>
2023-06-20 15:13:59 +0900

    Update NoticeServlet.java

--- src/src/servlet/NoticeServlet.java
+++ src/src/servlet/NoticeServlet.java
@@ -40,9 +40,9 @@ public class NoticeServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	/*rotected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
-	}
+	}*/
 
 }
