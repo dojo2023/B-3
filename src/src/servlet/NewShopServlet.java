c2aaf30dfb9a3edadc4860bcc30fb5b4c056a66c
dojo2023-seki <130331170+dojo2023-seki@users.noreply.github.com>
2023-06-21 15:24:42 +0900

    Update NewShopServlet.java

--- src/src/servlet/NewShopServlet.java
+++ src/src/servlet/NewShopServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class NewShopServlet
@@ -21,11 +22,11 @@ public class NewShopServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// もしもログインしていなかったらログインサーブレットにリダイレクトする
-		/*HttpSession session = request.getSession();
+		HttpSession session = request.getSession();
 		if (session.getAttribute("id_users") == null) {
 			response.sendRedirect("/hydrangea/LoginServlet");
 			return;
-		}*/
+		}
 		// 管理者用通知ページにフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/newshop.jsp");
 		dispatcher.forward(request, response);
