433dc07a8fd6c109b11a88dfce05418de8b5be03
dojo2023-seki <130331170+dojo2023-seki@users.noreply.github.com>
2023-06-21 10:33:54 +0900

    ファイル作成、削除

--- src/src/servlet/DeleteRequestServlet.java
+++ src/src/servlet/DeleteRequestServlet.java
@@ -1,11 +1,14 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class DeleteRequestServlet
@@ -18,8 +21,15 @@ public class DeleteRequestServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		if (session.getAttribute("id_users") == null) {
+			response.sendRedirect("/hydrangea/LoginServlet");
+			return;
+		}
+		// 管理者用通知ページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/delete_request.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	/**
