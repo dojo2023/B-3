afb96586ce60bb1868d7de2452edfdbe04bd38f5
dojo2023-ito-ma <130334764+dojo2023-ito-ma@users.noreply.github.com>
2023-06-21 14:00:14 +0900

    伊藤更新

--- src/src/servlet/HistoryServlet.java
+++ src/src/servlet/HistoryServlet.java
@@ -8,7 +8,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class HistoryServlet
@@ -17,27 +16,37 @@ import javax.servlet.http.HttpSession;
 public class HistoryServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// もしもログインしていなかったらログインサーブレットにリダイレクトする
-		HttpSession session = request.getSession();
-		if (session.getAttribute("id_users") == null) {
-			response.sendRedirect("/hydrangea/LoginServlet");
-			return;
-		}
+		//HttpSession session = request.getSession();
+		//if (session.getAttribute("id_users") == null) {
+			//response.sendRedirect("/hydrangea/LoginServlet");
+			//return;
+		//}
+
+		// 検索ページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/historyServlet.jsp");
+		dispatcher.forward(request, response);
+	}
+}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+/*	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
-
+*/
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	/*rotected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
-	}
+	}*/
 
-}
+
+
+
