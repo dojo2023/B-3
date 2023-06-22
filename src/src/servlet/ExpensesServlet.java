44bfa43ee48e92201d0cfdb3b086afbfa62d8036
dojo2023-ito-ma <130334764+dojo2023-ito-ma@users.noreply.github.com>
2023-06-22 17:10:16 +0900

    各テンプレサーブレット更新

--- src/src/servlet/ExpensesServlet.java
+++ src/src/servlet/ExpensesServlet.java
@@ -16,11 +16,14 @@ import javax.servlet.http.HttpServletResponse;
 public class ExpensesServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		//HttpSession session = request.getSession();
+		//if (session.getAttribute("id") == null) {
+			//response.sendRedirect("/hydrangea/LoginServlet");
+			//return;
+		//}
 
 		// 検索ページにフォワードする
 				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/expensesServlet.jsp");
@@ -34,30 +37,8 @@ public class ExpensesServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 
-		// もしもログインしていなかったらログインサーブレットにリダイレクトする
-				//HttpSession session = request.getSession();
-				//if (session.getAttribute("id") == null) {
-					//response.sendRedirect("/hydrangea/LoginServlet");
-					//return;
-				//}
-
-		// リクエストパラメータを取得する
-		request.setCharacterEncoding("UTF-8");
-
-	// 作成または戻るを行う
-	//BcDAO bDao = new BcDAO();
-	if (request.getParameter("SUBMIT").equals("作成")) {
-		// 経費申請書テンプレページにフォワードする
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/expensesServlet.jsp");
-		dispatcher.forward(request, response);
-	}
-	else if (request.getParameter("SUBMIT").equals("戻る")) {
-		// 結果ページにフォワードする
-		System.out.println(request.getParameter("SUBMIT"));
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/newpositionmemo.jsp");
-		dispatcher.forward(request, response);
 
 		}
 	}
 
-}
+
--- src/src/servlet/NotificationServlet.java
+++ src/src/servlet/NotificationServlet.java
@@ -16,11 +16,15 @@ import javax.servlet.http.HttpServletResponse;
 public class NotificationServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		//HttpSession session = request.getSession();
+		//if (session.getAttribute("id") == null) {
+			//response.sendRedirect("/hydrangea/LoginServlet");
+			//return;
+		//}
 
 		// 検索ページにフォワードする
 				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/notificationServlet.jsp");
@@ -28,48 +32,14 @@ public class NotificationServlet extends HttpServlet {
 
 	}
 
+
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
-		// もしもログインしていなかったらログインサーブレットにリダイレクトする
-				//HttpSession session = request.getSession();
-				//if (session.getAttribute("id") == null) {
-					//response.sendRedirect("/hydrangea/LoginServlet");
-					//return;
-				//}
-
-		// 送信を行う
-				//BcDAO bDao = new BcDAO();
-				if (request.getParameter("SUBMIT").equals("送信")) {
-					// 結果ページにフォワードする
-					System.out.println(request.getParameter("SUBMIT"));
-					RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/notificationServlet.jsp");
-					dispatcher.forward(request, response);
-				}
 
 
-				// リクエストパラメータを取得する
-				request.setCharacterEncoding("UTF-8");
-
-			// 保存またはリセットを行う
-			//BcDAO bDao = new BcDAO();
-			if (request.getParameter("SUBMIT").equals("保存")) {
-				// 役職メモページにフォワードする
-				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/positionmemoServlet.jsp");
-				dispatcher.forward(request, response);
-			}
-			else if (request.getParameter("SUBMIT").equals("追加")) {
-				// 結果ページにフォワードする
-				System.out.println(request.getParameter("SUBMIT"));
-				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/newpositionmemo.jsp");
-				dispatcher.forward(request, response);
-
-				}
 	}
-
-
-
 }
