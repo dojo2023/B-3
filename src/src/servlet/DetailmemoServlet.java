7172f58080050dad86f4e5214fde3a3c73cede00
dojo2023-ito-ma <130334764+dojo2023-ito-ma@users.noreply.github.com>
2023-06-28 11:00:59 +0900

    修正

--- src/src/servlet/DetailmemoServlet.java
+++ src/src/servlet/DetailmemoServlet.java
@@ -8,7 +8,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class DetailmemoServlet
@@ -23,8 +22,6 @@ public class DetailmemoServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/detailmemoServlet.jsp");
 		dispatcher.forward(request, response);
-		
-		
 	}
 
 	/**
@@ -34,12 +31,12 @@ public class DetailmemoServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 
-//		 もしもログインしていなかったらログインサーブレットにリダイレクトする
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
 				HttpSession session = request.getSession();
-				if (session.getAttribute("id_users") == null) {
-					response.sendRedirect("/hydrangea/LoginServlet");
-					return;
-				}
+				if (session.getAttribute("id") == null) {
+					//response.sendRedirect("/hydrangea/LoginServlet");
+					//return;
+				//}
 		// リクエストパラメータを取得する
 		request.setCharacterEncoding("UTF-8");
 		String id_memos = request.getParameter("ID_MEMOS");
